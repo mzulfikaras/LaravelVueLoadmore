177583d59b2abf4d9fb17629e5c9b153784e04a1
mzulfikaras <zulfikaralisunan96@gmail.com>
2020-05-19 01:24:19 +0700

    loadmore admin

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+
+
+class UserController extends Controller
+{
+    public function index(Request $request): JsonResponse
+    {
+        $users = \App\User::orderBy('name')->simplePaginate((int) $request->get('limit', 10));
+        return response()->json($users)->withCallback($request->callback);
+    }
+}
